3cfb99e3a1155a0ae64ef3dfcc48d62c6b8338dd
Hector Gómez Varela <hector@safe.global>
2022-07-01 11:43:02 +0200

    init basic models

--- financialadvisor/settings.py
+++ financialadvisor/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolios.apps.PortfoliosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'financialadvisor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'financial-advisor',
+        'USER': 'financial-advisor',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
@@ -113,7 +118,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-fi`les/
 
 STATIC_URL = 'static/'
 
--- portfolios/models.py
+++ portfolios/models.py
@@ -1,3 +1,24 @@
+from email.policy import default
+from pyexpat import model
 from django.db import models
+from django.forms import FloatField
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=6)
+    def __str__(self):
+        return self.name
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField('date created')
+    def __str__(self):
+        return self.name
+
+class Position(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    target_weight = models.FloatField()
+    shares = models.IntegerField(default=0)
+    def __str__(self):
+        return self.company + ' - ' + self.portfolio
