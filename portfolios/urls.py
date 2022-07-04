3959c46f803a0b588e97baf56e11834a92f1899c
Hector Gómez Varela <hector@safe.global>
2022-07-04 16:03:38 +0200

    fix bad import

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('companies/', views.companies_list),
     path('companies/<int:pk>', views.company_detail),
 ]
--- portfolios/views.py
+++ portfolios/views.py
@@ -1,5 +1,5 @@
-from django.views.decorators.csrf import api_view
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
